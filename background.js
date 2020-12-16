41b2d010e0105affeaf7661989b6dbe34ee01930
Nizzan Kruvi <>
2020-12-16 22:53:29 +0200

    add v1

---
+++ background.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(
+    tab.id,
+    {code: 'a = document.getElementsByClassName("_2Q3SY"); \
+        new_state = !a[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.hidden; \
+        for (let i = 0; i < a.length; i++) { \
+          a[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.hidden = new_state; \
+        } '});
+});
